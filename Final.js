857d01a9b3119bff5d0affb537639dda42b54f49
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-03 20:30:02 +0600

    problem4

--- Final.js
+++ Final.js
@@ -35,3 +35,17 @@ if(lastDay > 3 && lastDay <=30){
      
     }
 }
+
+
+/** Problem 04 - (Delete / Store) */
+var fileName= "pdfData.jpg";
+//write your code here
+if(fileName.startsWith('#')){
+    console.log('Store');
+}
+else if(fileName.endsWith('.pdf') || fileName.endsWith('.docx')){
+    console.log('Store');
+}
+else{
+    console.log('Delete');
+}
