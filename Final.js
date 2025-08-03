9d3c43ae9eb3b27886efa8e8bee4a8c4b2633f3f
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-03 20:08:10 +0600

    problem3

--- Final.js
+++ Final.js
@@ -18,3 +18,20 @@ else if(money >= 10000){
 else{
     console.log("Chocolate");
 }
+
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 6;
+//write your code here
+
+if(lastDay > 3 && lastDay <=30){
+    for( i = 1; i <= lastDay; i++){
+        if(i % 3 === 0){
+            console.log(i +' - '+'medicine')
+        }
+        else{
+            console.log(i +' - '+'rest')
+        }
+     
+    }
+}
---
+++ Problem-3.js
@@ -0,0 +1,15 @@
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 6;
+//write your code here
+
+if(lastDay > 3 && lastDay <=30){
+    for( i = 1; i <= lastDay; i++){
+        if(i % 3 === 0){
+            console.log(i +' - '+'medicine')
+        }
+        else{
+            console.log(i +' - '+'rest')
+        }
+     
+    }
+}
