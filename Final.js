238f5f3315b43fb1e431e24b960bf93f83ae1d2d
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-03 20:56:33 +0600

    problem5

--- Final.js
+++ Final.js
@@ -49,3 +49,11 @@ else if(fileName.endsWith('.pdf') || fileName.endsWith('.docx')){
 else{
     console.log('Delete');
 }
+
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhanku" , roll: 1014 ,department: "cse" };
+//write your code here
+
+var email = student.name + student.roll  + '.' + student.department + '@ph.ac.bd';
+console.log(email);
